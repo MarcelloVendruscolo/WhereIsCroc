7cbe0299b068365584e998122d5fa9978796a78e
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2020-09-17 20:54:10 +0200

    Initialisation of the R script.

---
+++ makeMoves.R
@@ -0,0 +1,13 @@
+#List all the packages available
+library()
+
+#List all the packages currently loaded
+search()
+
+#Load DeliveryMan package for this session
+library(WheresCroc)
+
+makeMoves <- function() {
+  
+  return(0)
+}
