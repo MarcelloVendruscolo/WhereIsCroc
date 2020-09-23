b7cfaa7a280ddb9dedbf7da406d31fc32c04b44b
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2020-09-23 11:01:21 +0200

    Search algorithm improved to surpass par performance.

--- makeMoves.R
+++ makeMoves.R
@@ -7,7 +7,7 @@ search()
 #Load DeliveryMan package for this session
 library(WheresCroc)
 
-#Function for calculating probabilities for salinity, phosphate and nitrogen of all waterholes given the readings from Croc's current location
+#Help function for calculating the observable probabilities (salinity, phosphate and nitrogen) of each waterhole given the observations from Croc's current location
 current_waterholesProbabilites <- function(croc_locationReadings, waterholes_distributions) {
   
   observation_matrix <- matrix(data = 0, nrow = 40, ncol = 40)
@@ -39,10 +39,10 @@ current_waterholesProbabilites <- function(croc_locationReadings, waterholes_dis
   return(observation_matrix)
 }
 
+#Help function to calculate the transition matrix considering a uniform distribution
 compute_transitionMatrix <- function(edges_matrix) {
   
   transitionProbabilities_Matrix <- matrix(0, nrow = 40, ncol = 40)
-  
   counter_neighbours <- seq(1, 1, length.out = 40)
 
   for (i in 1:dim(edges_matrix)[1]) {
@@ -60,8 +60,9 @@ compute_transitionMatrix <- function(edges_matrix) {
   return(transitionProbabilities_Matrix)
 }
 
+#Help function to find the neighbours (adjacent nodes) of a given node
 compute_neighbours <- function(waterhole, edges_matrix) {
-  neighbours <- seq(0, 0, lenght.out = 0)
+  neighbours <- c()
   for (i in 1:dim(edges_matrix)[1]) {
     if (edges_matrix[i,1] == waterhole) {
       neighbours[length(neighbours) + 1] <- edges_matrix[i,2]
@@ -70,34 +71,67 @@ compute_neighbours <- function(waterhole, edges_matrix) {
       neighbours[length(neighbours) + 1] <- edges_matrix[i,1]
     }
   }
-
-  return(neighbours[c(2:length(neighbours))])
+  return(neighbours)
 }
 
 calculate_shortestPath <- function(origin_node, destination_node, edges_matrix) {
   
-  node <- list('number' = 0, 'parent' = 0, 'visited' = F)
-  graph <- list()
-  queue <- c()
+  #Variable to store the distance between destination and origin nodes. All edges are considered to have same weight
+  shortest_distance <- 0
   
+  #Elementary node data structure
+  node <- list('number' = 'undefined', 'parent' = 'undefined', 'distance' = 'undefined')
+  #Graph data structure as list to represent the map
+  graph <- list()
+  #Queue data structure as list to optimize the BFS algorithm
+  queue <- list()
+  
+  #Fulfill the graph data structure with 40 nodes
   for (index in 1:40) {
     node$number <- index
     graph[[index]] <- node
   }
   
-  current_node <- graph[[origin_node]]
+  #Set the current ranger location as the start point and en-queue it.
+  graph[[origin_node]]$parent <- origin_node
+  graph[[origin_node]]$distance <- 0
+  queue[[1]] <- graph[[origin_node]]
   
-  if (current_node == destination_node) {
-    return(current_node$number);
-  }
-  else {
+  while (length(queue) != 0) {
     
-    children <- compute_neighbours(current_node$number, edges_matrix)
-    for (index in 1:length(children)) {
-      queue[length(queue) + 1] <- children[index]
+    #Create a temporary current_node holding the first element of the queue and de-queue it.
+    current_node <- queue[[1]]
+    queue <- queue[-1]
+    
+    #If the destination is reached, iterate back through the path to find out the best move from current ranger position
+    if (current_node$number == destination_node) {
+      shortest_distance <- current_node$distance
+      #If the destination waterhole is far away (distance > 2), move as fast as possible to the destination area
+      if (shortest_distance > 2) {
+        while (current_node$distance != 2) {
+          current_node <- graph[[current_node$parent]]
+        }
+        next_waterhole_moves <- c(graph[[current_node$parent]]$number, current_node$number)
+      } 
+      #If the destination waterhole is close (distance <= 2), move but also search the waterholes
+      else {
+        while (current_node$parent != origin_node) {
+          current_node <- graph[[current_node$parent]]
+        }
+        next_waterhole_moves <- c(current_node$number, 0)
+      }
+      return(next_waterhole_moves)
     }
-    while (length(queue) != 0) {
-      
+    #If the destination is not reached yet, explore the node and en-queue its adjacent nodes.
+    else {
+      children <- compute_neighbours(current_node$number, edges_matrix)
+      for (index in 1:length(children)) {
+        if (graph[[children[index]]]$distance == 'undefined') {
+          graph[[children[index]]]$distance <- current_node$distance + 1
+          graph[[children[index]]]$parent <- current_node$number
+          queue[[length(queue) + 1]] <- graph[[children[index]]]
+        }
+      }
     }
   }
 }
@@ -113,46 +147,49 @@ calculate_shortestPath <- function(origin_node, destination_node, edges_matrix) 
 #waterholes_distributions$nitrogen is a matrix giving the nitrogen's mean and sd at each waterhole
 makeMoves <- function(move_information, croc_locationReadings, humans_location, edges_matrix, waterholes_distributions) {
   
-  #Vector data structure to store probability information about each of the waterholes 
+  #Matrix data structure to store probability information about each of the waterholes
   waterholes <- matrix(0, nrow = 1, ncol = 40, byrow = T)
-  transition_matrix <- compute_transitionMatrix(edges_matrix)
+  
+  #Calculate observable waterholes' probability based on the readings of each round.
+  observableProbabilities_matrix <- current_waterholesProbabilites(croc_locationReadings, waterholes_distributions)
   most_probable_waterhole <- 1
   
-  observationProbabilities_matrix <- current_waterholesProbabilites(croc_locationReadings, waterholes_distributions)
-  
   #At S0, the probability of each waterhole is the product of its salinity, phosphate and nitrogen probabilities given Croc's location readings
-  if (move_information$mem$status == 0) {
-    for (index in 1:40) {
-      waterholes[1,index] <- observationProbabilities_matrix[index,index]
-    }
+  if (move_information$mem$status == 0 | move_information$mem$status == 1) {
+    
+    #Compute transition matrix when a new game starts and store in memory
+    transition_matrix <- compute_transitionMatrix(edges_matrix)
+    move_information$mem$transition_matrix <- transition_matrix
+    
+    #Create a first matrix where all waterholes have the same probability, as no there is previous knowledge about the system
+    first_matrix <- matrix((1/40), nrow = 1, ncol = 40, byrow = TRUE)
+    
+    #Calculate the new probability of each waterhole by forwarding probabilities from the first_matrix according to HMM
+    waterholes <- first_matrix %*% move_information$mem$transition_matrix %*% observableProbabilities_matrix
   }
   
-  #At S1, the most probable waterhole will a neighbour of the most probable waterhole at S0 or itself, depending on the transition matrix
-  if (move_information$mem$status != 0) {
-    
-    waterholes <- move_information$mem$probabilitiesVector * transition_matrix * observationProbabilities_matrix
-    
-    #Updated the game status to a number different of 0 and 1
-    move_information$mem$status <- -1
+  #After the first round, the most probable waterhole is calculated by forwarding probabilities and considering the new observable reading probabilities
+  if (move_information$mem$status == -1) {
+    waterholes <- move_information$mem$waterholesProbabilities %*% move_information$mem$transition_matrix %*% observableProbabilities_matrix
   }
   
   #Update waterholes probabilities based on the information inferred by the location or death of the two tourists
   for (tourist in 1:2) {
+    #If a tourist has died in previous rounds, there is nothing to do about it anymore
+    if (is.na(humans_location[tourist]) == TRUE) {
+    }
     #If a tourist remains alive after a round, Croc is definitely not at the tourist current waterhole
-    if (humans_location[tourist] > 0) {
+    else if (humans_location[tourist] > 0) {
       waterholes[1,humans_location[tourist]] <- 0
     }
     #If a tourist dies during a round, Croc is definitely at the tourist's death location
-    if (humans_location[tourist] < 0) {
+    else if (humans_location[tourist] < 0) {
       for (index in 1:40) {
         waterholes[1,index] <- 0
       }
-      waterholes[1,humans_location[tourist]] <- 1
+      waterholes[1,(humans_location[tourist]*(-1))] <- 1
     }
   }
-  
-  move_information$mem$probabilitiesVector <- waterholes
-  
   
   #Based on the probabilities, find the most probable waterhole in which Croc is currently at
   for (index in 1:40) {
@@ -161,12 +198,27 @@ makeMoves <- function(move_information, croc_locationReadings, humans_location, 
     }
   }
   
-  cat("Most probable waterhole: ", most_probable_waterhole)
+  #cat("Most probable waterhole: ", most_probable_waterhole, "\n")
   
-  next_waterhole <- calculate_shortestPath(humans_location[3], most_probable_waterhole, edges_matrix)
-  move_information$moves[next_waterhole, 0]
+  #Computes the next 2 moves based on current location, destination waterhole and edges connecting waterholes
+  move_information$moves <- calculate_shortestPath(humans_location[3], most_probable_waterhole, edges_matrix)
   
+  #If one of the moves is to search, the probability of such waterhole can be updated to zero
+  #Croc will certainly not be there is the game proceeds to the next round
+  if (move_information$moves[2] == 0) {
+    waterholes[1,move_information$moves[1]] <- 0
+  }
+
+  #Store information in memory for access during the upcoming round
+  move_information$mem$waterholesProbabilities <- waterholes
+  
+  #Update the game status to a number different of 0 and 1 after the first round
+  if (move_information$mem$status == 0 | move_information$mem$status == 1) {
+    move_information$mem$status <- -1
+  }
+
   return(move_information)
 }
 
 runWheresCroc(makeMoves, doPlot = T, showCroc = T, pause = 1, verbose = T, returnMem = F, mem = NA)
+testWC(makeMoves, verbose = 1, returnVec = FALSE, n = 500, seed = 21, timeLimit = 300)
